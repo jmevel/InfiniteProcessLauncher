cdd69b60631097546611f56f1ef51bb3b81a6b12
Jerome <jeromemevel@hotmail.com>
2018-10-24 22:45:53 +0800

    initial commit

---
+++ InfiniteProcessHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace InfiniteProcessLauncher
+{
+    public class InfiniteProcessHelper
+    {
+        private string _programName { get; set; }
+
+        private string _arguments { get; set; }
+
+        private Process _process { get; set; }
+
+        public event EventHandler ProcessLaunched;
+
+        public InfiniteProcessHelper(string programPath, string arguments = null)
+        {
+            _programName = programPath;
+            _arguments = arguments;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                KillProcessIfRunning();
+                CreateProcessAndWait();
+            }
+            catch(Exception)
+            {
+                KillProcessIfRunning();
+            }
+            finally
+            {
+                Run();
+            }
+        }
+
+        //Just extra security but should never happen
+        private void KillProcessIfRunning()
+        {
+            if (_process != null && _process.HasExited == false)
+            {
+                _process.Kill();
+                _process = null;
+            }
+        }
+
+        private void CreateProcessAndWait()
+        {
+            _process = new Process();
+            _process.StartInfo.FileName = _programName;
+            _process.StartInfo.Arguments = _arguments;
+
+            //No need to subscribe to the event. The WaitForExit is going to end and the Run() is alled again in the Finally
+            //_process.Exited += _process_Exited;
+
+            _process.Start();
+            ProcessLaunched(this, new EventArgs());
+            _process.WaitForExit();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace InfiniteProcessLauncher
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var programPath = args[0];
+                var stringPath = programPath.Replace(@"\", @"\\");
+                stringPath = $"\"{stringPath}\"";
+
+                string arguments = null;
+                string stringArguments = null;
+                var hideWindow = false;
+
+                if (args.Length >= 2)
+                {
+                    try
+                    {
+                        //If it successes, no arguments have been passed to the program called
+                        hideWindow = Convert.ToBoolean(args[1]);
+                    }
+                    catch (Exception)
+                    {
+                        arguments = args[1];
+                        stringArguments = arguments.Replace(@"\", @"\\");
+                        stringArguments = $"\"{stringArguments}\"";
+                    }
+                }
+                if(args.Length >= 3)
+                {
+                    hideWindow = Convert.ToBoolean(args[2]);
+                }
+                if(hideWindow)
+                {
+                    var handle = GetConsoleWindow();
+                    ShowWindow(handle, SW_HIDE);
+                }
+
+                try
+                {
+                    var infiniteProcessHelper = new InfiniteProcessHelper(programPath, stringArguments);
+                    infiniteProcessHelper.ProcessLaunched += InfiniteProcessHelper_ProcessLaunched;
+                    Console.WriteLine($"Executing: \"{programPath}\" \"{arguments}\"");
+                    infiniteProcessHelper.Run();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Usage: dotnet InfiniteProcessLauncher.dll \"Path\\To\\Program.exe\" [\"Optional Parameters\"] [hide window (default: false)]");
+                Console.Read();
+            }   
+        }
+
+        private static void InfiniteProcessHelper_ProcessLaunched(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Process launched at {DateTime.Now}");
+        }
+    }
+}
